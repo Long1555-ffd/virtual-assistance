061ed0fc2d52f7ca2f389e6dd0a0c6d0c10ce231
minhquang5014 <minhquang5014@gmail.com>
2024-10-23 21:58:05 +0700

    testing with the mistral model here

--- model/mistral-7b.py
+++ model/mistral-7b.py
@@ -5,11 +5,14 @@ device = "cuda"
 
 model = AutoModelForCausalLM.from_pretrained("Open-Orca/Mistral-7B-OpenOrca").to(device)
 tokenizer = AutoTokenizer.from_pretrained("Open-Orca/Mistral-7B-OpenOrca")
-
-inputs = tokenizer("Who are you?", return_tensors="pt").to(device)
-outputs = model.generate(
-    **inputs, max_new_tokens=256, use_cache=True, do_sample=True,
-    temperature=0.2, top_p=0.95
-)
-text = tokenizer.batch_decode(outputs)[0]
-print(text)
+print(torch.cuda.is_available())
+try:
+    inputs = tokenizer("Who are you?", return_tensors="pt").to(device)
+    outputs = model.generate(
+        **inputs, max_new_tokens=256, use_cache=True, do_sample=True,
+        temperature=0.2, top_p=0.95
+    )
+    text = tokenizer.batch_decode(outputs)[0]
+    print(text)
+except Exception as e:
+    print(f"An error occurred {e}")
