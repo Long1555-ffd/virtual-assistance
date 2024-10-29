830896d91b32294c5a18203a9443c9efb570ca3e
Long1555-ffd <long2794813@gmail.com>
2024-10-29 22:10:33 +0700

    testing w the 2.7B params model

--- model/meta-llama.py
+++ model/meta-llama.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
+import torch
 # Choose the model you want
 model_name = "facebook/opt-2.7b"  # or any of the other options
 
@@ -7,16 +7,19 @@ local_dir = "./opt-2.7B/models--facebook--opt-2.7b/snapshots/905a4b602cda5c501f1
 # Load the tokenizer and model from hugging face
 # tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="opt-2.7B")
 # model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir="opt-2.7B")
+print(torch.cuda.is_available())
+# # load the model from local dir
+# tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="./opt-2.7B")
+# model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir="./opt-2.7B")
 
-# load the model from local dir
-tokenizer = AutoTokenizer.from_pretrained(local_dir, local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(local_dir)
 model = AutoModelForCausalLM.from_pretrained(local_dir)
 
-input_text = "The possibilities of artificial intelligence are endless,"
+input_text = "Tell me about the future of AI and robotics, can AI be the big brain for robots to process their surroundings?"
 inputs = tokenizer(input_text, return_tensors="pt")
 
 # Generate text
-output = model.generate(**inputs, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2)
+output = model.generate(**inputs, max_length=200, num_return_sequences=1, no_repeat_ngram_size=2)
 
 # Decode and print the generated text
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
