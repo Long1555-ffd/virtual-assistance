60e396ba8a2d9e77dd63db360ce1ce68ad838b70
minhquang5014 <minhquang5014@gmail.com>
2024-11-28 11:16:02 +0700

    output mp3 audio

--- TTS-model/gtts_model.py
+++ TTS-model/gtts_model.py
@@ -6,5 +6,7 @@ def speak_text(text):
     tts.save("output.mp3")
     os.system("mpg321 output.mp3")  # Play audio (ensure mpg321 is installed)
 
-text = "Hello, how are you today?"
+text = """There are still over 50 days left until Donald Trump takes office, but he’s already laid the ground for a trade war that could shake the global economy.
+Trump announced on Monday that he will sign an executive order placing a 25% tariff on all imports from Canada and Mexico, along with an additional 10% tariff on imports from China, in purported retaliation for drugs and migrants crossing US borders.
+"""
 speak_text(text)
